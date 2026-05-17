0f5c1b39f61055ba881cff934e1de192b8b87cf8
jeffreysu1355 <jeffreysu1355@gmail.com>
2026-05-17 00:55:47 -0700

    feat: add WatchlistFlag component

---
+++ src/components/WatchlistFlag/WatchlistFlag.tsx
@@ -0,0 +1,33 @@
+import styles from './WatchlistFlag.module.css';
+
+interface WatchlistFlagProps {
+  isWatchlisted: boolean;
+  isOwnGym: boolean;
+  onToggle: () => void;
+}
+
+export function WatchlistFlag({ isWatchlisted, isOwnGym, onToggle }: WatchlistFlagProps) {
+  if (!isWatchlisted) {
+    return (
+      <button
+        type="button"
+        className={styles.flag}
+        title="Add to watchlist"
+        onClick={onToggle}
+      >
+        🏴
+      </button>
+    );
+  }
+
+  return (
+    <button
+      type="button"
+      className={`${styles.flagActive} ${isOwnGym ? styles.flagGreen : styles.flagRed}`}
+      title="Remove from watchlist"
+      onClick={onToggle}
+    >
+      🏴
+    </button>
+  );
+}
